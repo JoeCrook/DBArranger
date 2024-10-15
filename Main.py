eb2a86260d3ba36faa315e1f02b7a724524fbeae
Joe Crook <josephcrook.95@gmail.com>
2024-10-15 12:36:40 +0100

    Testing window creation

--- Main.py
+++ Main.py
@@ -1,1 +1,16 @@
-from Functions import *
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Hello World")
+
+
+def handle_button_press(event):
+    window.destroy()
+
+
+button = tk.Button(text="My simple app.")
+button.bind("", handle_button_press)
+button.pack()
+
+# Start the event loop.
+window.mainloop()

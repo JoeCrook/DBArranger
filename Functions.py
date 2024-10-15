46ace38dc06cb847bf96081298ca240c5e30a455
Joe Crook <josephcrook.95@gmail.com>
2024-10-15 12:34:44 +0100

    Commented out some code not needed

--- Functions.py
+++ Functions.py
@@ -383,10 +383,10 @@ def checkAnother(item):
 
 
 # Gathers which function is wanted, and runs the function to find/start it
-findFunction(input(
-    "Function type required (\"Find Tag\", \"Select Section\", \"DI\" or \"Tesys\"): ").lower().replace(" ", ""))
+# findFunction(input(
+#    "Function type required (\"Find Tag\", \"Select Section\", \"DI\" or \"Tesys\"): ").lower().replace(" ", ""))
 
 # Loops until the user states otherwise
-while checkAnother("function") == True:
-    findFunction(input(
-        "Function type required (\"Find Tag\", \"Select Section\" or \"DI\"): ").lower().replace(" ", ""))
+# while checkAnother("function") == True:
+#    findFunction(input(
+#        "Function type required (\"Find Tag\", \"Select Section\" or \"DI\"): ").lower().replace(" ", ""))

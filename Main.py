4fbe7200529126c207cae1e850ec15d1f40c2c36
Joe Crook <josephcrook.95@gmail.com>
2024-10-15 12:50:00 +0100

    Created some test functions

--- Main.py
+++ Main.py
@@ -16,9 +16,18 @@ center_y = int(root.winfo_screenheight()/2 - root_height / 2)
 root.geometry(f'{root_width}x{root_height}+{center_x}+{center_y}')
 
 
+def button1():
+    print("Button 1")
+
+
+def button2():
+    print("Button 2")
+
+
 # place a label on the root window
-message = ttk.Label(root, text="Hello, World!")
-message.pack()
+message = ttk.Label(root, text="Hello, World!").pack()
+button1 = ttk.Button(root, text="Button 1", command=button1).pack()
+button2 = ttk.Button(root, text="Button 2", command=button2).pack()
 
 # keep the window displaying
 root.mainloop()

f4159be9ae1c251386a516a1ecd3ffebd78c9439
Joe Crook <josephcrook.95@gmail.com>
2024-10-15 13:03:49 +0100

    Added the actual functions for use

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from Functions import *
 
 
 root = tk.Tk()
@@ -16,18 +17,13 @@ center_y = int(root.winfo_screenheight()/2 - root_height / 2)
 root.geometry(f'{root_width}x{root_height}+{center_x}+{center_y}')
 
 
-def button1():
-    print("Button 1")
-
-
-def button2():
-    print("Button 2")
-
-
 # place a label on the root window
-message = ttk.Label(root, text="Hello, World!").pack()
-button1 = ttk.Button(root, text="Button 1", command=button1).pack()
-button2 = ttk.Button(root, text="Button 2", command=button2).pack()
+message = ttk.Label(root, text="Pick a function:").pack()
+button1 = ttk.Button(root, text="Find Tag", command=findTag).pack()
+button2 = ttk.Button(root, text="Select Section", command=selectSection).pack()
+button3 = ttk.Button(root, text="Create DI", command=createDI).pack()
+button4 = ttk.Button(root, text="Create TESYS", command=tesys).pack()
+
 
 # keep the window displaying
 root.mainloop()
